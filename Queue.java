264ff7bf0e7a9e51045d862e4f835b4b5f2cf98b
Eann Lao <43785692+LuraLotus@users.noreply.github.com>
2019-02-22 23:52:43 +0800

    Create Queue.java

---
+++ Queue.java
@@ -0,0 +1,28 @@
+public class Queue
+{
+  public Node head;
+  public Node tail;
+  public int size;
+
+  public Queue()
+  {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+  //Adds an activity
+  public void Add(String input)
+  {
+
+  }
+  //Deletes an activity
+  public void Delete()
+  {
+
+  }
+  //Goes to next activity
+  public void Next()
+  {
+    
+  }
+}
