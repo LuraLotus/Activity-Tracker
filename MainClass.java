02ec01f02fdbb98d10c46a4404cf915b5ec4cd4d
Eann Lao <43785692+LuraLotus@users.noreply.github.com>
2019-02-25 14:07:21 +0800

    Update MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -3,10 +3,43 @@
  * Section: A
 **/
 
+import java.util.Scanner;
+
 class MainCLass
 {
   public static void main(String[] args)
   {
-    
+    Queue q = new Queue();
+    int c = 0;
+    System.out.print("Current Activity: ");
+    if(Queue.current == null)
+    {
+      System.out.println("None");
+    }
+    else
+    {
+      System.out.println(Queue.current);
+    }
+    System.out.println("What would you like to do?");
+    System.out.println("0 - Exit");
+    System.out.println("1 - Next Activity");
+    System.out.println("2 - New Activity");
+    System.out.println("3 - Delete Activity");
+    System.out.println("4 - Reorder Activity");
+    System.out.print("Input: ");
+    Scanner input = new Scanner(System.in);
+    c = input.nextInt();
+    switch(c)
+    {
+      case 0: Exit();
+      case 1: q.Next();
+      case 2: System.out.println("Enter activity to add: ");
+              q.Add(input.nextLine());
+      case 3: System.out.println("Which activity would you like to remove?");
+              q.Display();
+              System.out.print("Input: ");
+              q.Delete(input.nextInt());
+      case 4:
+    }
   }
 }
