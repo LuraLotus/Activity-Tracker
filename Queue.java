3cb101ed2dad0aadd278d234866f00a9eeca62e4
debsterscarlet <47839083+debsterscarlet@users.noreply.github.com>
2019-03-08 09:44:59 +0800

    Update Queue.java

--- Queue.java
+++ Queue.java
@@ -19,7 +19,21 @@ public class Queue
   //Deletes an activity (displays all added activities for the user to choose from)
   public void Delete()
   {
-
+	System.out.println("Select an activity to delete from the list: ");
+        String del = in.readLine();
+		File SourceFile = new File(input);
+        Scanner input = new Scanner(SourceFile);
+        PrintWriter output = new PrintWriter(SourceFile);
+        while (input.hasNext())
+		{ 
+            String x = input.nextLine();
+            String y = x.replaceAll(del, "");
+            output.println(y);
+        }
+        input.close();
+        output.close();
+        System.out.println("Output: o" + input);
+  }
   }
   //Goes to next activity
   public void Next()
