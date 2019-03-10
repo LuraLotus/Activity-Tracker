66016fc76b5706273096c337b7a212c315f93f4d
Eann Lao <43785692+LuraLotus@users.noreply.github.com>
2019-03-10 16:24:19 +0800

    Update MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -6,51 +6,59 @@
 import java.util.Scanner;
 import java.io.*;
 
-class MainCLass
+class MainClass
 {
   public static void main(String[] args) throws IOException
   {
     Queue q = new Queue();
     Scanner in = new Scanner(new File("activities.txt"));
     PrintWriter out = null;
-    while(input.hasNextLine())
+    while(in.hasNextLine())
     {
-      list.Add(input.nextLine());
+      q.Add(in.nextLine());
     }
-    System.out.print("Current Activity: ");
-    if(Queue.current == null)
-    {
-      System.out.println("None");
-    }
-    else
-    {
-      System.out.println(Queue.current);
-    }
-    System.out.println("What would you like to do?");
-    System.out.println("0 - Exit");
-    System.out.println("1 - Next Activity");
-    System.out.println("2 - New Activity");
-    System.out.println("3 - Delete Activity");
-    System.out.println("4 - Reorder Activity");
-    System.out.print("Input: ");
+
     Scanner input = new Scanner(System.in);
+    Scanner add = new Scanner(System.in);
     int c = 0;
-    c = input.nextInt();
     boolean bool = true;
     while(bool == true)
     {
+      System.out.print("Current Activity: ");
+      if(q.head == null)
+      {
+        System.out.println("None");
+      }
+      else
+      {
+        System.out.println(q.head.data);
+      }
+      System.out.println("What would you like to do?");
+      System.out.println("0 - Exit");
+      System.out.println("1 - Next Activity");
+      System.out.println("2 - New Activity");
+      System.out.println("3 - Delete Activity");
+      System.out.println("4 - Reorder Activity");
+      System.out.println("5 - Display all activities");
+      System.out.print("Input: ");
+      c = input.nextInt();
       switch(c)
       {
         case 0: bool = false;
+                break;
         case 1: q.Next();
+                break;
         case 2: System.out.println("Enter activity to add: ");
-                q.Add(input.nextLine());
-        case 3: System.out.println("Which activity would you like to remove?");
-                q.Display();
-                System.out.print("Input: ");
-                q.Delete(input.nextInt());
+                String addin = add.nextLine();
+                q.Add(addin);
+                break;
+        case 3: q.Delete();
+                break;
         case 4: System.out.println("List of Activities: ");
-                q.Display();
+                q.Reorder();
+                break;
+        case 5: q.Display();
+                break;
       }
     }
   }
